0ea74a0452d71185bc64ec061b93e50a745aed95
Luis M <lmbrmiguelito@hotmail.com>
2022-02-25 10:50:19 -0500

    Add server file

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express'); 
+var cors = require('cors');
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const app = express(); 
+app.use(cors());
+const port = process.env.PORT || 5000; 
+const EXPIRATION_TIME = 1;
+const cacheDatabase = {};
+const discountCodes = [];
+let counter = 0;
+
+Date.prototype.addHours= function(h) {
+  this.setHours(this.getHours()+h);
+  return this;
+}
+
+// create a GET route
+app.get('/serial_number/:serialNumber', (req, res) => {
+  const serialNumber = decodeURIComponent(req.params?.serialNumber); 
+  console.log({serialNumber});
+  const discountCode = getDiscountCode(serialNumber);
+  console.log({ discountCode });
+  res.send({ discountCode }); 
+});
+
+const getDiscountCode = serialNumber => {
+  console.log('getDiscountCode');
+  if(!cacheDatabase[serialNumber]) return 'Invalid serial number'
+  console.log(cacheDatabase[serialNumber]);
+  if(Object.keys(cacheDatabase[serialNumber]).length === 0) {
+    const discountCode = discountCodes[counter % discountCodes.length];
+    counter += 1;
+    cacheDatabase[serialNumber] = { discountCode, expirationDate: Date.now() + EXPIRATION_TIME * 3600 * 1000 }
+    return discountCode;
+  }
+  console.log(Date.now())
+  if(cacheDatabase[serialNumber].expirationDate > Date.now()) {
+    return 'Serial Number Redeemed'
+  }
+  else {
+    cacheDatabase[serialNumber] = {};
+    return getDiscountCode(serialNumber);
+  }
+}
+
+const loadSerialNumbers = () => {
+  fs.createReadStream('serial_numbers.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row);
+    if(row.serial_number && row.serial_number !== '') cacheDatabase[row.serial_number] = {};
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+    app.listen(port, () => {console.log(`Listening on port ${port}`)}); 
+  });
+}
+
+const loadDiscountCodes = () => {
+  fs.createReadStream('discount_codes.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    //console.log(row);
+    if(row.code && row.code !== '') discountCodes.push(row.code);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+    loadSerialNumbers();
+  });
+}
+
+loadDiscountCodes();
